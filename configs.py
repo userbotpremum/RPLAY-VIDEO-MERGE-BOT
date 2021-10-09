ff1f1b7fcb93753e6ba87433758b7db993c6d34d
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-09 19:05:56 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,24 +8,24 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     SESSION_NAME = os.environ.get("SESSION_NAME", "RPLAY-VIDEO-MERGE-BOT")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL" "-1001210904908" )
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001210904908' )
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
     MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 15))
-    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
-    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
+    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "2ab52c1ef915e52f4ee8" )
+    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "zwgdM4xjXpsYPAP" )
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://rplayvcbot:1rplay2@cluster0.n7alv.mongodb.net/cluster0?retryWrites=true&w=majority" )
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1990460616)
 
     START_TEXT = """
-Hi Unkil, I am Video Merge Bot!
+Hi `{event.sender.first_name}`, I am RPLAY ™ Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 
-Made by @AbirHasan2005
+Made by @renishrplay
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "Video Merged by @{}\n\nMade by @renishrplay"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
