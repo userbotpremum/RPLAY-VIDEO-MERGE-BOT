605224839112c0821012ea6aad9b0efdc2ceb73c
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-09 22:57:56 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,20 +2,20 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = os.environ.get("API_ID", default="7434892" )
+    API_HASH = os.environ.get("API_HASH", default="b645623710413894a1c0e084450876e2" )
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Rplay-video-merger-bot)
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", default="-1001576361605" )
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", default="-1001576361605" )
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
     MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
-    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
-    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
+    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", default="™2ab52c1ef915e52f4ee8" )
+    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", default="™zwgdM4xjXpsYPAP" )
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://rplayvcbot:1rplay2@cluster0.n7alv.mongodb.net/cluster0?retryWrites=true&w=majority" )
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1990460616))
 
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
