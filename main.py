891df8bafbbe6afeaf1cc4cbc0f65b9e7abfedc0
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-10 14:05:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -368,8 +368,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
             text=Config.START_TEXT,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer - @RPLAY", url="https://t.me/renishrplay")], [InlineKeyboardButton("MAKE OWN", url="https://github.com/Rplayoriginal/RPLAY-VIDEO-MERGE-BOT"), InlineKeyboardButton("CONTACT BOT", url="https://t.me/Rplay_renish_bot")], [InlineKeyboardButton("MOVIE CHANNEL", url="https://t.me/rplaymovie"),InlineKeyboardButton("MOVIE STICKERS", url="https://t.me/addstickers/RPLAY_stickers")], [InlineKeyboardButton("SUPPORT GROUP", url="https://t.me/rplay_support"),InlineKeyboardButton("DONATE", url="https://www.paypal.com/signin?returnUri=https%3A%2F%2Fwww.paypal.com%2Fmyaccount%2Ftransfer%2Fhomepage%2Fexternal%2Fprofile%3FflowContextData%3DMTmBt")]]),
-        )
-    )
+            disable_web_page_preview=True
         )
     elif "showThumbnail" in cb.data:
         db_thumbnail = await db.get_thumbnail(cb.from_user.id)
