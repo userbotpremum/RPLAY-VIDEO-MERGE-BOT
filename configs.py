31f66428b01eb926467520f958133e8aa84f66f1
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-12 18:00:35 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,7 +17,8 @@ class Config(object):
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1990460616))
 
-    START_TEXT = """f "HELLO 👋 **NAME:** [{cb.from_user.first_name}](tg://user?id={str(cb.from_user.id)})\n**your Username:** `{cb.from_user.username}`\n**your UserID:** `{cb.from_user.id}`\nI am RPLAY ™ Video Merge Bot!\nI can Merge Multiple Videos into One Video.\n Video Formats should be same.\nMade by @renishrplay"
+    START_TEXT = """ 
+         f"HELLO 👋 **NAME:** [{cb.from_user.first_name}](tg://user?id={str(cb.from_user.id)})\n**your Username:** `{cb.from_user.username}`\n**your UserID:** `{cb.from_user.id}`\nI am RPLAY ™ Video Merge Bot!\nI can Merge Multiple Videos into One Video.\n Video Formats should be same.\nMade by @renishrplay"
 """ 
     CAPTION = "Video Merged by @{}\n\nMade by @renishrplay"
     PROGRESS = """
