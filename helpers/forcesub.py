ca1da4457b153be30d7a439a27f492b226f93d73
userbotpremum <59136793+userbotpremum@users.noreply.github.com>
2021-10-31 02:22:30 +0530

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -21,7 +21,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/BullMovies_Team).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -46,7 +46,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/BullMovies_Team).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
