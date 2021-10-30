273688065be304d7c24b959164cac4dffaa9cd69
userbotpremum <59136793+userbotpremum@users.noreply.github.com>
2021-10-31 02:19:10 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID", "7434892" )
-    API_HASH = os.environ.get("API_HASH", "b645623710413894a1c0e084450876e2" )
+    API_ID = os.environ.get("API_ID", "6701181" )
+    API_HASH = os.environ.get("API_HASH", "9df7dd72e31ae336edf34dfdf406b14b" )
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     SESSION_NAME = os.environ.get("SESSION_NAME", "video-merger-bot" )
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001576361605" )
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001597887921" )
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001556437539" )
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001556437539" )
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
     MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 15))
@@ -15,14 +15,14 @@ class Config(object):
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "zwgdM4xjXpsYPAP" )
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://rplayvcbot:1rplay2@cluster0.n7alv.mongodb.net/cluster0?retryWrites=true&w=majority" )
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1990460616))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1167961858))
 
     START_TEXT ="""
-Hi 👋, I am **RPLAY ™** `Renish Video Merge` @Rplay_video_mergebot!
+Hi 👋, I am **COBRA MOVIES** `Video Merge` @videomerge842_bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
-Made by @renishrplay 
+Made by @BullMovies_Team 
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @renishrplay"
+    CAPTION = "Video Merged by @{}\n\nMade by @BullMovies_Team"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
